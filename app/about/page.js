203ad651633bfbac2380f7d9b8740cf02ddf6444
AngelinaMartynova7 <angelinaangelina531@gmail.com>
2024-06-22 23:37:10 +0300

    new file:   app/about/page.js 	new file:   app/api/data/route.js 	new file:   app/components/Header.js 	new file:   app/components/Post.js 	modified:   app/globals.css 	modified:   app/layout.js 	modified:   app/page.js 	new file:   app/post/[id]/page.js 	modified:   jsconfig.json 	new file:   next.config.js 	modified:   package-lock.json 	modified:   package.json

---
+++ app/about/page.js
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "Страница про нас",
+  description: "Страница про нас",
+  keywords: '',
+};
+
+export default function About() {
+    return (
+      <div>
+            <h1>Страница про нас</h1>
+            <p>Lorem</p>
+      </div>
+   
+  )
+}
